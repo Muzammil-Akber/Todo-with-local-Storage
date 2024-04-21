381e105cc80d8991de40a6b6e805b8014280af0d
Muzammil Akber <muzammilraza365@gmail.com>
2024-04-21 12:27:24 +0500

    Every thing Perfect

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,6 @@ function App() {
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                      <h4>dhbdhbdjhvbjfbvjh</h4>
                         {/* Todo form goes here */}                
                         <TodoForm />
                     </div>
