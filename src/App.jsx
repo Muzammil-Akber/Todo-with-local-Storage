736b7f7a8442cfee5713c2cc5dd0e0176c340b86
Muzammil-second <167800739+Muzammil-second@users.noreply.github.com>
2024-04-23 09:40:05 +0500

    add one commit

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,9 @@ function App() {
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */}                
+                        {/* Todo form goes here */}   
+                      <h4>
+                      cfjdcmsdvbmshdvnsdbvcmshndbsx,djhsbdmnxbdfvdhbvmnfvbd,jfhbdmnf</h4>
                         <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
